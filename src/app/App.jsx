0445e399ee79ded09ff809a8fac2220f549e854f
Tulio Ferreira <tuliof210@gmail.com>
2021-02-22 10:36:37 -0300

    set color

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,8 +6,32 @@ import Card from './components/card/Card';
 export default props => {
   return (
     <div className="app-wrapper">
-      <Card />
-      <Card />
+      <h3>Exercicios React-redux</h3>
+      <div className="line">
+        <Card color="red" title="Card 01">
+          A
+        </Card>
+      </div>
+      <div className="line">
+        <Card color="blue" title="Card 02">
+          B
+        </Card>
+      </div>
+      <div className="line">
+        <Card color="green" title="Card 03">
+          C
+        </Card>
+      </div>
+      <div className="line">
+        <Card color="yellow" title="Card 04">
+          D
+        </Card>
+      </div>
+      <div className="line">
+        <Card color="pink" title="Card 05">
+          E
+        </Card>
+      </div>
     </div>
   );
 };
--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -1,6 +1,17 @@
 import './Card.scss';
 import React from 'react';
 
+const getClass = (current, color = '') => {
+  return `${current} ${color}`;
+};
+
 export default props => {
-  return <div className="card-wrapper">Card</div>;
+  return (
+    <div className={getClass('card-wrapper', props['color'])}>
+      <div className="header">
+        <span className="title">{props['title']}</span>
+      </div>
+      <div className="content">{props.children}</div>
+    </div>
+  );
 };
